b7d38f8721a5416c0d81f16b5220eea48665ca60
azmusgb <36898133+azmusgb@users.noreply.github.com>
2026-09-11 12:48:08 -0400

    Add family runtime boundary

---
+++ src/runtime/family.js
@@ -0,0 +1,7 @@
+// Family/person/tree interaction boundary.
+import '../../v12-6.js';
+import '../../v12-6-1.js';
+import '../../v12-6-2.js';
+import '../../v12-7.js';
+import '../../v12-9.js';
+import '../../v12-9-1.js';
